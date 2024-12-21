316859c266ce79b1c319ebae1e46a8d7de70584b
JRDP Team <136267216+jrdpteam@users.noreply.github.com>
2024-12-21 22:14:26 +0100

    Add files via upload

---
+++ installer.py
@@ -0,0 +1,103 @@
+import os
+import time
+import subprocess
+from colorama import Fore, Style, init
+
+init(autoreset=True)
+
+def clear_screen():
+    os.system("clear")
+
+def print_ascii_art():
+    print(f'''{Fore.RED}
+                  _
+   {Fore.YELLOW}! INSTALLER !{Fore.RED} | |
+ __   _____  _ __| |_ _____  __
+ \\ \\ / / _ \\| '__| __/ _ \\ \\/ /
+  \\ V / (_) | |  | ||  __/>  <    {Fore.MAGENTA}v3.2{Fore.RED}
+   \\_/ \___/|_|   \\__\\___/_/\\_\\
+    ''' + Style.RESET_ALL)
+
+def install_vortex():
+    clear_screen()
+    print_ascii_art()
+    print("\nInstalling vortex...")
+    time.sleep(0.1)
+    os.system("chmod 777 vortex.py")
+    os.system("chmod +x vortex.py")
+    os.system("mkdir -p /usr/share/vortex")
+    os.system("cp vortex.py /usr/share/vortex/vortex.py")
+
+    cmnd = '#! /bin/sh \n exec python3 /usr/share/vortex/vortex.py "$@"'
+    with open('/usr/bin/vortex', 'w') as file:
+        file.write(cmnd)
+    
+    os.system("chmod +x /usr/bin/vortex && chmod +x /usr/share/vortex/vortex.py")
+
+    desktop_entry = '''
+[Desktop Entry]
+Version=1.0
+Name=vortex
+Comment=vortex
+Exec=python3 /usr/share/vortex/vortex.py
+Icon=utilities-terminal
+Terminal=true
+Type=Application
+Categories=Utility;
+'''
+
+    desktop_file_path = '/usr/share/applications/vortex.desktop'
+    with open(desktop_file_path, 'w') as file:
+        file.write(desktop_entry)
+
+    os.system(f"chmod +x {desktop_file_path}")
+
+    success_message = "vortex has been installed successfully.\nFrom now on, just type 'vortex -h' in the terminal."
+    subprocess.Popen(["zenity", "--info", "--text", success_message, "--title", "Success"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+def uninstall_vortex():
+    clear_screen()
+    print_ascii_art()
+    print("\nUninstalling vortex...")
+    time.sleep(0.4)
+
+    os.system("rm -r /usr/share/vortex")
+    os.system("rm /usr/bin/vortex")
+
+    os.system("rm /usr/share/applications/vortex.desktop")
+
+    success_message = "vortex has been uninstalled successfully."
+    subprocess.Popen(["zenity", "--info", "--text", success_message, "--title", "Success"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+def main_menu():
+    clear_screen()
+    print_ascii_art()
+    print("\nMenu:")
+    print("1. Install vortex")
+    print("2. Uninstall vortex")
+    print("3. Exit")
+
+    choice = input("\nEnter your choice (1/2/3): ")
+    return choice
+
+def check_sudo():
+    return os.geteuid() == 0
+
+if __name__ == "__main__":
+    while True:
+        if not check_sudo():
+            print("Please run the installer with sudo privileges.")
+            break
+
+        choice = main_menu()
+
+        if choice == '1':
+            install_vortex()
+        elif choice == '2':
+            uninstall_vortex()
+        elif choice == '3':
+            print("Exiting program. Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please choose again.")
+            time.sleep(1)
---
+++ vortex.py
@@ -0,0 +1,209 @@
+import os
+import sys
+import random
+import shutil
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.hazmat.primitives import hashes
+import argparse
+from colorama import init, Fore, Style
+
+init(autoreset=True)
+
+ascii_art = r"""
+                  _            
+        v3.2     | |           
+ __   _____  _ __| |_ _____  __
+ \ \ / / _ \| '__| __/ _ \ \/ /
+  \ V / (_) | |  | ||  __/>  < 
+   \_/ \___/|_|   \__\___/_/\_\
+"""
+
+def print_ascii_art():
+    print(Fore.RED + ascii_art)
+
+def colorful_help():
+    print(Fore.YELLOW + "vortex v3.2    " + Fore.MAGENTA + "by JRDP Team 2024")
+    print(Fore.CYAN + "\nUsage:")
+    print(Fore.GREEN + "  vortex.py [-h] -f FILE [-l PASSWORD_LENGTH] [-t CHARS [CHARS ...]]")
+    print(Fore.GREEN + "            [-c CUSTOM_PASSWORD] [-e] [-d]\n")
+    print(Fore.CYAN + "Options:")
+    print(Fore.BLUE + "  -h, --help               " + Fore.RESET + "Show this help message and exit")
+    print(Fore.BLUE + "  -f FILE, --file FILE     " + Fore.RESET + "File to encrypt/decrypt")
+    print(Fore.BLUE + "  -l PASSWORD_LENGTH, --password-length PASSWORD_LENGTH" + Fore.RESET + " Length of password")
+    print(Fore.BLUE + "  -t CHARS [CHARS ...], --chars CHARS [CHARS ...]" + Fore.RESET + " Characters to use for password generation (letter, number, extra)")
+    print(Fore.BLUE + "  -c CUSTOM_PASSWORD, --custom-password CUSTOM_PASSWORD" + Fore.RESET + " Use custom password")
+    print(Fore.BLUE + "  -e, --encrypt            " + Fore.RESET + "Encrypt file")
+    print(Fore.BLUE + "  -d, --decrypt            " + Fore.RESET + "Decrypt file")
+    print(Fore.CYAN + "\nContact: " + Fore.RESET + "https://jrdpteam.netlify.app")
+    print(Fore.CYAN + "GitHub:  " + Fore.RESET + "https://github.com/jrdpteam")
+
+print_ascii_art()
+
+def generate_password(length, chars):
+    char_set = ''
+    if 'letter' in chars:
+        char_set += 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    if 'number' in chars:
+        char_set += '0123456789'
+    if 'extra' in chars:
+        char_set += '_-'
+    
+    if not char_set:
+        print(Fore.RED + 'Error: No valid characters specified for password generation.')
+        sys.exit(1)
+        
+    password = ''.join(random.choice(char_set) for _ in range(length))
+    return password
+
+def encrypt_file(file_path, password):
+    try:
+        with open(file_path, 'rb') as f:
+            data = f.read()
+    except IOError as e:
+        print(Fore.RED + f'Error reading file: {e}')
+        sys.exit(1)
+    
+    salt = os.urandom(16)
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=1000000,
+        backend=default_backend()
+    )
+    key = kdf.derive(password.encode())
+    iv = os.urandom(16)
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+    padder = padding.PKCS7(128).padder()
+    padded_data = padder.update(data) + padder.finalize()
+    encrypted_data = encryptor.update(padded_data) + encryptor.finalize()
+    
+    original_name = os.path.basename(file_path)
+    original_name_bytes = original_name.encode('utf-8')
+    original_name_length = len(original_name_bytes).to_bytes(4, 'big')
+    
+    encrypted_file_path = file_path + '.vrtx'
+    try:
+        with open(encrypted_file_path, 'wb') as f:
+            f.write(salt)
+            f.write(iv)
+            f.write(original_name_length)
+            f.write(original_name_bytes)
+            f.write(encrypted_data)
+    except IOError as e:
+        print(Fore.RED + f'Error writing encrypted file: {e}')
+        sys.exit(1)
+    
+    print(Fore.GREEN + f'File encrypted and saved as {encrypted_file_path}')
+
+
+def decrypt_file(file_path, password):
+    if not file_path.endswith('.vrtx'):
+        print(Fore.RED + 'Error: File is not encrypted.')
+        sys.exit(1)
+    
+    try:
+        with open(file_path, 'rb') as f:
+            salt = f.read(16)
+            iv = f.read(16)
+            original_name_length = int.from_bytes(f.read(4), 'big')
+            original_name = f.read(original_name_length).decode('utf-8')
+            encrypted_data = f.read()
+    except IOError as e:
+        print(Fore.RED + f'Error reading encrypted file: {e}')
+        sys.exit(1)
+    
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=1000000,
+        backend=default_backend()
+    )
+    key = kdf.derive(password.encode())
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    decryptor = cipher.decryptor()
+    decrypted_padded_data = decryptor.update(encrypted_data) + decryptor.finalize()
+    unpadder = padding.PKCS7(128).unpadder()
+    
+    try:
+        decrypted_data = unpadder.update(decrypted_padded_data) + unpadder.finalize()
+    except ValueError as e:
+        print(Fore.RED + 'Error: Incorrect password.')
+        sys.exit(1)
+    
+    try:
+        with open(original_name, 'wb') as f:
+            f.write(decrypted_data)
+    except IOError as e:
+        print(Fore.RED + f'Error writing decrypted file: {e}')
+        sys.exit(1)
+    
+    print(Fore.GREEN + f'File decrypted and saved as {original_name}')
+
+def save_password(password):
+    try:
+        with open('password.vortex', 'w') as f:
+            f.write(password)
+    except IOError as e:
+        print(Fore.RED + f'Error saving password: {e}')
+        sys.exit(1)
+
+def main():
+    parser = argparse.ArgumentParser(description=Fore.YELLOW + 'vortex v3.2    ' + Fore.MAGENTA + 'by JRDP Team 2019-2024\n' +
+                                     Fore.CYAN + "Contact: " + Fore.RESET + "https://jrdpteam.netlify.app\n" +
+                                     Fore.CYAN + "GitHub:  " + Fore.RESET + "https://github.com/jrdpteam", 
+                                     formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('-f', '--file', help=Fore.BLUE + 'File to encrypt/decrypt' + Fore.RESET, required=True)
+    parser.add_argument('-l', '--password-length', type=int, help=Fore.BLUE + 'Length of password' + Fore.RESET)
+    parser.add_argument('-t', '--chars', help=Fore.BLUE + 'Characters to use for password generation (letter, number, extra)' + Fore.RESET, nargs='+')
+    parser.add_argument('-c', '--custom-password', help=Fore.BLUE + 'Use custom password' + Fore.RESET)
+    parser.add_argument('-e', '--encrypt', help=Fore.BLUE + 'Encrypt file' + Fore.RESET, action='store_true')
+    parser.add_argument('-d', '--decrypt', help=Fore.BLUE + 'Decrypt file' + Fore.RESET, action='store_true')
+    
+    if len(sys.argv) == 1:
+        colorful_help()
+        sys.exit(1)
+
+    args = parser.parse_args()
+
+    if not os.path.exists(args.file):
+        print(Fore.RED + f'Error: File {args.file} does not exist.')
+        sys.exit(1)
+
+    if not (args.encrypt or args.decrypt):
+        print(Fore.RED + 'Error: You must specify either -e or -d.')
+        sys.exit(1)
+
+    if args.custom_password:
+        if args.password_length or args.chars:
+            print(Fore.RED + 'Error: -c cannot be used with -l or -t.')
+            sys.exit(1)
+        password = args.custom_password
+        save_password(password)
+    elif args.encrypt:
+        if not args.password_length:
+            print(Fore.RED + 'Error: You must specify password length (-l) for encryption.')
+            sys.exit(1)
+        if not args.chars:
+            print(Fore.RED + 'Error: You must specify characters to use for password generation (-t).')
+            sys.exit(1)
+        password = generate_password(args.password_length, args.chars)
+        print(Fore.GREEN + 'Generated password: ' + password)
+        save_password(password)
+    elif args.decrypt:
+        password = input(Fore.YELLOW + "Enter the password for decryption: ")
+
+    if args.encrypt:
+        encrypt_file(args.file, password)
+        print(Fore.GREEN + 'File encrypted successfully.')
+    elif args.decrypt:
+        decrypt_file(args.file, password)
+        print(Fore.GREEN + 'File decrypted successfully.')
+
+if __name__ == '__main__':
+    main()
